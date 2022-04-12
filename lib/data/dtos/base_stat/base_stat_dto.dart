ba7becaaa48b2c721ba8497149805f4070f3b9eb
unknown <viniciusps01@hotmail.com>
2022-04-12 01:20:59 -0300

    Added dto for BaseStatEntity

---
+++ lib/data/dtos/base_stat/base_stat_dto.dart
@@ -0,0 +1,4 @@
+import 'package:desafio_grupo_hostaraguaia/data/dtos/json_serialization_dto.dart';
+import 'package:desafio_grupo_hostaraguaia/domain/entities/base_stat_entity.dart';
+
+abstract class BaseStatDto implements JsonSerializationDto<BaseStatEntity> {}
